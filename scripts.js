68b208738da32055bd40d4603ca57f4384695288
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-09-02 10:01:38 +0700

    test

--- scripts.js
+++ scripts.js
@@ -44,29 +44,26 @@ const createPeerConnection = async (offerObject) => {
             peerconnection.addTrack(track, localStream);
         });
 
-        peerconnection.addEventListener('icecandidate',e=>{
-            // console.log('........Ice candidate found!......')
-            // console.log(e)
-            if(e.candidate){
-                socket.emit('sendIceCandidateToSignalingServer',{
+        peerconnection.addEventListener('icecandidate', e => {
+            if (e.candidate) {
+                socket.emit('sendIceCandidateToSignalingServer', {
                     iceCandidate: e.candidate,
                     iceUserName: offerObject ? 'receiver' : 'sender',
-                    didIOffer:offerObject ? false : true,
-                })   
-                // console.log('ice candidate')
-                // console.log(e.candidate) 
+                    didIOffer: offerObject ? false : true,
+                });
             }
-        })
+        });
 
         peerconnection.addEventListener('track', (event) => {
-            // console.log('The answer track media is:', event);
+            console.log('Track event: Adding remote track');
             event.streams[0].getTracks().forEach((track) => {
                 remoteStream.addTrack(track);
             });
         });
 
         if (offerObject) {
-            peerconnection.setRemoteDescription(offerObject.offer);
+            console.log('Setting remote description for received offer');
+            await peerconnection.setRemoteDescription(offerObject.offer);
         }
         resolve();
     });
@@ -82,13 +79,9 @@ const fetchUserMedia = async () => {
     }
 }
 
-
-const addAnswer = async(offerObj)=>{
-    //addAnswer is called in socketListeners when an answerResponse is emitted.
-    //at this point, the offer and answer have been exchanged!
-    //now CLIENT1 needs to set the remote
-    await peerconnection.setRemoteDescription(offerObj.answer)
-    // console.log(peerConnection.signalingState)
+const addAnswer = async (offerObj) => {
+    console.log('Setting remote description for received answer');
+    await peerconnection.setRemoteDescription(offerObj.answer);
 }
 
 const answerOffer = async (offer) => {
@@ -100,10 +93,39 @@ const answerOffer = async (offer) => {
     offer.answer = answer;
     socket.emit('answer', offer);
 }
-const addNewIceCandidate = iceCandidate=>{
-    peerconnection.addIceCandidate(iceCandidate)
-    console.log("======Added Ice Candidate======")
-    console.log(iceCandidate)
+
+const addNewIceCandidate = async (iceCandidate) => {
+    try {
+        await peerconnection.addIceCandidate(iceCandidate);
+        console.log("======Added Ice Candidate======");
+    } catch (error) {
+        console.error('Error adding received ICE candidate:', error);
+    }
 }
 
 document.querySelector('#call').addEventListener('click', call);
+socket.on('availableOffer', (offer) => {
+    createOfferEl(offer);
+});
+
+socket.on('newOfferWaiting', (offer) => {
+    createOfferEl(offer);
+});
+
+socket.on('receiverIceCandidate', (iceCandidate) => {
+    addNewIceCandidate(iceCandidate);
+});
+
+socket.on('answerResponse', (offer) => {
+    addAnswer(offer);
+});
+
+const createOfferEl = (offer) => {
+    const answerEl = document.querySelector('#answer')
+    offer.forEach(element => {
+        const newOfferEl = document.createElement('div')
+        newOfferEl.innerHTML = `<button class="btn btn-primary">Answer ${element.offerUserName}</button>`
+        newOfferEl.addEventListener('click', () => answerOffer(element))
+        answerEl.appendChild(newOfferEl)
+    });
+};
